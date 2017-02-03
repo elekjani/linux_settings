e148ad30a1e7ff873f3dfd9adcb936d7f194e02a
Janos Elek <janos.elek@intec.ugent.be>
2017-02-03 17:31:40 +0100

    Add submodule init for Vundle

--- install.sh
+++ install.sh
@@ -2,9 +2,16 @@
 
 git_root=`pwd`/`dirname $0`
 
+pushd $git_root
+
+cd Vundle.vim
+git submodule init
+git submodule update
 cp $git_root/.tmux.conf ~
 cp $git_root/vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle 2>/dev/null
 cp -R $git_root/Vundle.vim/ ~/.vim/bundle
 cat $git_root/.ps1 >> ~/.bashrc
 vim -c VundleInstall -c "qa!"
+
+popd
